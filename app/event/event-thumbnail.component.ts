4b5180730900d8d8ef8c463e033eee80d25f9978
Daniel Garcia <daniel.garcia@banlinea.com>
2017-05-25 17:50:40 -0500

    Buena res

--- app/event/event-thumbnail.component.ts
+++ app/event/event-thumbnail.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     selector: 'event-thumbnail',
     template: `
     <div class="well thumbnail">
-        <h2>{{event.date}}</h2>
+        <h2>{{event.name}}</h2>
         <div>Date: {{event.date}}</div>
         <div>Time: {{event.time}}</div>
         <div>Price: \${{event.price}}</div>
